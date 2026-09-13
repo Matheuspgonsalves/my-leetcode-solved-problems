5a4534e6d34dff6ecf14581cc36edabc3278af0e
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-09-13 11:17:59 -0300

    solved: queue exercise number 933

--- LeetCode/933.number-of-recent-calls/src/RecentCounter.ts
+++ LeetCode/933.number-of-recent-calls/src/RecentCounter.ts
@@ -2,10 +2,20 @@
  * Counts how many requests landed within the trailing 3000ms window.
  */
 export class RecentCounter {
-  private requests: number[] = [];
+  private requests: number[];
+
+  constructor(){
+    this.requests = [];
+  }
 
   ping(t: number): number {
     this.requests.push(t);
+
+    while(t - 3000 > this.requests[0]!) {
+      this.requests.shift();
+    }
+
+    
     return this.requests.length;
   }
 }
